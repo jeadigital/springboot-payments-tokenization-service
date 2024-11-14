9fcf234c308a88feccb3c67cc6787c127adb475c
Jagadeesh <blockchian@jeadigital.com>
2024-11-14 21:55:04 +0530

    Initial commit

---
+++ src/main/java/com/jea/payments/token/SpringbootPaymentsTokenizationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jea.payments.token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPaymentsTokenizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPaymentsTokenizationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jea/payments/token/SpringbootPaymentsTokenizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jea.payments.token;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPaymentsTokenizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
